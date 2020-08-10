e23192ce9b297f2a15298d455a0cb7b39834042c
Adam Adolfo <adamadolfo8@gmail.com>
2020-08-10 17:22:32 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
     element_index = 0 
     new_string = ""
     while element_index < src[row_index].count do
-      if src[row_index][element_index] == string
+      if src[row_index][element_index] == ""
         new_string << src[row_index][element_index]
       end
       element_index += 1 
